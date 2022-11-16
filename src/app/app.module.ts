130cb3662de3b85f2b47ed885b4e79d4b2e58327
João Neto <joao.neto96@souunit.com.br>
2022-11-15 21:57:23 -0300

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FileSizePipe } from './demos/pipes/filmes/filesize.pipe';
+import { FilmesComponent } from './demos/pipes/filmes/filmes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FilmesComponent,
+    FileSizePipe
   ],
   imports: [
     BrowserModule,
